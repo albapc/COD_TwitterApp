6e8a2d0dee7b7918f8c7e945836cd43cb725c00c
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-03-26 10:06:44 +0100

    creación del proyecto

---
+++ src/proyectotwitter/ProyectoTwitter.java
@@ -0,0 +1,17 @@
+package proyectotwitter;
+
+
+/**
+ *
+ * @author aperezcesar
+ */
+public class ProyectoTwitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
